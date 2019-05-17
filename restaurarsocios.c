6ebff033de01e709d2261a7b6fb3ac65e003e9d6
Anniapgarcia <47822120+Anniapgarcia@users.noreply.github.com>
2019-05-17 17:55:25 +0200

    Eliminacion de espacios innecesarios
    se han eliminados los espacios innecesarios

--- restaurarsocios.c
+++ restaurarsocios.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 
-
 typedef struct
 {
 	char nombre[100];
@@ -11,48 +10,41 @@ typedef struct
 }socia;
 
 
-
 main()
 {
-FILE *pf;
-socia u[10]={{"Sandra","Rebollo",1234},{"Annia","Malvar",2345}};
-int i;
-int dimension=2;
+	FILE *pf;
+	socia u[10]={{"Sandra","Rebollo",1234},{"Annia","Malvar",2345}};
+	int i;
+	int dimension=2;
 
 
-pf=fopen("sociod.txt", "w");
-fprintf(pf, "%d", dimension);
-fclose(pf);
+	pf=fopen("sociod.txt", "w");
+	fprintf(pf, "%d", dimension);
+	fclose(pf);
 
-pf=fopen("socios.txt", "wb");
-for (i=0; i<dimension; i++)
-{
+	pf=fopen("socios.txt", "wb");
+	for (i=0; i<dimension; i++)
 		fwrite(&u[i], sizeof(socia), 1, pf);
-}
-fclose(pf);
+	fclose(pf);
 							
-								
-								
+
+	//Leer socio
+	pf=fopen("sociod.txt", "r");
+	fscanf(pf,"%d", &dimension);
+	printf("%d\n",dimension);
+	fclose(pf);
 
 
-//Leer socio
-pf=fopen("sociod.txt", "r");
-fscanf(pf,"%d", &dimension);
-printf("%d\n",dimension);
-fclose(pf);
+	pf=fopen("socios.txt", "rb");
+	for (i=0; i<dimension; i++)
+	{
+		fread(&u[i], sizeof(socia), 1, pf);
 
+		printf("Nombre: %s.\n", u[i].nombre);
+		printf("Apellido: %s.\n", u[i].apellido);
+		printf("Codigo: %d.\n", u[i].codigo);
 
-pf=fopen("socios.txt", "rb");
-for (i=0; i<dimension; i++)
-{
-fread(&u[i], sizeof(socia), 1, pf);
-
-printf("Nombre: %s.\n", u[i].nombre);
-printf("Apellido: %s.\n", u[i].apellido);
-printf("Codigo: %d.\n", u[i].codigo);
-
-}
-fclose(pf);								
-								
-								
+	}
+	fclose(pf);								
+																
 }
